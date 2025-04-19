b237ac33bff9f40959b3e78e5ad596ed20b12e54
unknown <designwithtawhid@gmail.com>
2025-04-19 21:52:29 +0600

    update all jobs

--- src/pages/AllJobs.jsx
+++ src/pages/AllJobs.jsx
@@ -34,22 +34,20 @@ const AllJobs = () => {
             </select>
           </div>
 
-          <form>
-            <div className='flex p-1 overflow-hidden border rounded-lg    focus-within:ring focus-within:ring-opacity-40 focus-within:border-blue-400 focus-within:ring-blue-300'>
-              <input
-                className='px-6 py-2 text-gray-700 placeholder-gray-500 bg-white outline-none focus:placeholder-transparent'
-                type='text'
-                onChange={e => setSearch(e.target.value)}
-                name='search'
-                placeholder='Enter Job Title'
-                aria-label='Enter Job Title'
-              />
+          <div className='flex p-1 overflow-hidden border rounded-lg    focus-within:ring focus-within:ring-opacity-40 focus-within:border-blue-400 focus-within:ring-blue-300'>
+            <input
+              className='px-6 py-2 text-gray-700 placeholder-gray-500 bg-white outline-none focus:placeholder-transparent'
+              type='text'
+              onChange={e => setSearch(e.target.value)}
+              name='search'
+              placeholder='Enter Job Title'
+              aria-label='Enter Job Title'
+            />
 
-              <button className='px-1 md:px-4 py-3 text-sm font-medium tracking-wider text-gray-100 uppercase transition-colors duration-300 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:bg-gray-600 focus:outline-none'>
-                Search
-              </button>
-            </div>
-          </form>
+            <button className='px-1 md:px-4 py-3 text-sm font-medium tracking-wider text-gray-100 uppercase transition-colors duration-300 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:bg-gray-600 focus:outline-none'>
+              Search
+            </button>
+          </div>
           <div>
             <select
               name='category'
